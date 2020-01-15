a18fb274c81cf1c3880c02305fab8d370714233e
lokesh764 <59922579+lokesh764@users.noreply.github.com>
2020-01-15 19:33:42 +0530

    instance.tf
    this file will support the aws ec2 creation

---
+++ instance.tf
@@ -0,0 +1,26 @@
+# Defining master inside the public subnet
+resource "aws_instance" "node01" {
+   ami  = "${var.ami}"
+   instance_type = "t2.micro"
+   key_name = "mykey"
+   subnet_id = "${aws_subnet.subnet1.id}"
+   vpc_security_group_ids = ["${aws_security_group.allow_https_only.id}"]
+   user_data = "${file("starter.sh")}"
+
+  tags {
+    Name = "node01"
+  }
+}
+
+# Defining slave inside the private subnet
+resource "aws_instance" "node02" {
+   ami  = "${var.ami}"
+   instance_type = "t2.micro"
+   key_name = "mykey"
+   subnet_id = "${aws_subnet.subnet1.id}"
+   vpc_security_group_ids = ["${aws_security_group.allow_https_only.id}"]
+
+  tags {
+    Name = "node02"
+  }
+}
